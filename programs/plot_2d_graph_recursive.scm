bbf27f8e91a47e950ac7f23e943e0158c1ce319a
naru-jpn <tus.naru@gmail.com>
2015-07-12 18:13:51 +0900

    add program plot_2d_graph_recursive

---
+++ programs/plot_2d_graph_recursive.scm
@@ -0,0 +1,31 @@
+(define device (make-graphics-device (car (enumerate-graphics-types))))
+(clear)
+(write-string ";How to use:\n")
+(write-string ";  (plot-2d f(x) max-x max-y delta title)\n")
+(write-string ":  Type '(graphics-close device)' when you close graphic window.\n\n")
+
+(define (plot-2d f max-x max-y delta title)
+    ; clear
+    (graphics-clear device)
+    ; draw title, x-axis, y-axis
+    (graphics-draw-text device -0.96 0.94 title)
+    (graphics-draw-line device -1 0 1 0)
+    (graphics-draw-text device 0.94 0.04 (number->string max-x 10))
+    (graphics-draw-line device 0 -1 0 1)
+    (graphics-draw-text device 0.04 0.94 (number->string max-y 10))
+    (graphics-draw-text device 0.02 -0.06 "O")
+    ; plot graph
+    (define (next x) (+ x delta))
+    (define (draw-line x1 x2)
+        (define (scale-x _x) (/ _x max-x))
+        (define (scale-y _y) (/ _y max-y))
+        (graphics-draw-line device (scale-x x1) (scale-y (f x1)) (scale-x x2) (scale-y (f x2)))
+        x1)
+    (define (recursive-draw-line x)
+        (cond
+            ((>= x max-x) x)
+            (else (draw-line x (recursive-draw-line (next x))))))
+
+    (recursive-draw-line (- max-x))
+    (string-append "Drawn graph named '" title "'."))
+
